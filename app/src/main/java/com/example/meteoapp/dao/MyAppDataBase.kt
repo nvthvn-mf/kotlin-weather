453c151ce727ef9ce965a744a08d05ddb83c4e0d
Mufuta Mulamba Nathan <nathanmf06@gmail.com>
2026-01-25 19:31:22 +0100

    Correctif

--- app/src/main/java/com/example/meteoapp/dao/MyAppDataBase.kt
+++ app/src/main/java/com/example/meteoapp/dao/MyAppDataBase.kt
@@ -1,8 +1,6 @@
 package com.example.meteoapp.dao
 
 import androidx.room.Database
-import androidx.room.Entity
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.meteoapp.entity.CityEntity
 
