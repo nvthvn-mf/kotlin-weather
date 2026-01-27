c29f9051321534474e2795e40da3a79d6636c20f
Mufuta Mulamba Nathan <nathanmf06@gmail.com>
2026-01-27 23:18:59 +0100

    Nettoyage du code

--- app/src/main/java/com/example/meteoapp/AppDatabase.kt
+++ app/src/main/java/com/example/meteoapp/AppDatabase.kt
@@ -6,7 +6,6 @@ import com.example.meteoapp.dao.CityDao
 import com.example.meteoapp.entity.CityEntity
 
 @Database(entities = [CityEntity::class], version = 1)
-//@ConstructedBy(AppDatabaseConstructor::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun getDao(): CityDao
 }
