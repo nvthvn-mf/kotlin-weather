fa2f6f119bd4e912ca334c92d226385999c161e3
Mufuta Mulamba Nathan <nathanmf06@gmail.com>
2026-01-28 23:52:38 +0100

    Correctif

--- app/src/main/java/com/example/meteoapp/dao/MyAppDataBase.kt
+++ app/src/main/java/com/example/meteoapp/dao/MyAppDataBase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.meteoapp.entity.CityEntity
 
-@Database (entities = [CityEntity::class], version = 1)
-abstract class MyAppDataBase: RoomDatabase(){
+@Database(entities = [CityEntity::class], version = 1)
+abstract class MyAppDataBase : RoomDatabase() {
     abstract fun cityDao(): CityDao
 }
