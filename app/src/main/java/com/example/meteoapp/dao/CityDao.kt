131f5ae9b44a1fef15380c988ef46d9ea70efd94
Mufuta Mulamba Nathan <nathanmf06@gmail.com>
2026-01-28 23:52:55 +0100

    Correctif

--- app/src/main/java/com/example/meteoapp/dao/CityDao.kt
+++ app/src/main/java/com/example/meteoapp/dao/CityDao.kt
@@ -1,20 +1,21 @@
 package com.example.meteoapp.dao
+
 import androidx.room.*
 import com.example.meteoapp.entity.CityEntity
 
 @Dao
-interface CityDao{
+interface CityDao {
     @Insert
-     fun insert(city: CityEntity)
+    fun insert(city: CityEntity)
 
     @Query("SELECT * FROM city_table ORDER BY name ASC")
-     fun getAllCities(): List<CityEntity>
+    fun getAllCities(): List<CityEntity>
 
     @Query("SELECT * FROM city_table WHERE id=:cityId")
     suspend fun find(cityId: Int): CityEntity?
 
     @Delete
-    suspend fun delete (city: CityEntity)
+    suspend fun delete(city: CityEntity)
 
     @Update
     suspend fun update(city: CityEntity)
